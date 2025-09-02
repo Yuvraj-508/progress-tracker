2fdd6cfbe2125774cb250551ba52dd9977cc09e8
Yuvraj Singh <yuvrajsingh@Yuvrajs-MacBook-Air.local>
2025-09-02 15:01:42 +0530

    url

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 await connectDB();
 
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173','https://progress-tracker-dun.vercel.app'],
     credentials: true
   }));
 
